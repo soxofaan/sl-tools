184fc2e5668e53efb933e7d2c75b3d66f1ca9580
Stefaan Lippens <stefaan.lippens@elis.ugent.be>
2009-08-18 17:19:20 +0200

    refactored to if __name__ construct

--- platform_info.py
+++ platform_info.py
@@ -5,11 +5,15 @@ Use the platform module to query information about the platform:
 call all public functions in the module.
 '''
 
-import platform
+def main():
+    import platform
 
-for name in dir(platform):
-    if not name.startswith('_'):
-        try:
-            print "%s(): %s" % (name, platform.__dict__[name]())
-        except TypeError:
-            pass
+    for name in dir(platform):
+        if not name.startswith('_'):
+            try:
+                print "%s(): %s" % (name, platform.__dict__[name]())
+            except TypeError:
+                pass
+
+if __name__ == '__main__':
+    main()
