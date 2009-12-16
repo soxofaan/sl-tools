b118ce77fe523d687c66fc528c5d71d50b14d5a6
Stefaan Lippens <stefaan.lippens@elis.ugent.be>
2009-12-16 16:35:00 +0100

    a bit of refactoring and added command line options

--- histogram.py
+++ histogram.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python
+
 '''
 Created on Dec 16, 2009
 
-@author: slippens
+@author: Stefaan Lippens
 
 Observe data from the standard input (words, lines, numerical data)
 and build a histogram from it
 '''
+
 import optparse
 import sys
 
@@ -39,6 +42,28 @@ class ObjectHistogram(dict):
             for word in line.split():
                 self.observe(word)
 
+    def observe_from_sources(self, sources, observe_lines=False):
+        '''
+        Observe data from a list of sources.
+
+        @param sources a list of sources: file-like objects
+            or a file name string.
+        @param observe_lines whether or not lines (instead of
+            words) should be observed.
+        '''
+        for source in sources:
+            if isinstance(source, str):
+                f = open(source, 'r')
+            else:
+                f = source
+            if observe_lines:
+                self.observe_lines_from_file(f)
+            else:
+                self.observe_words_from_file(f)
+            if isinstance(source, str):
+                f.close()
+
+
     def ordered_items(self):
         '''
         Return an ordered list of histogram items
@@ -60,7 +85,8 @@ class ObjectHistogram(dict):
         total_count = sum(self.values())
         for obj, count in items[:limit]:
             bar = '#' * int(20.0 * count / max_count)
-            out.write('%20s %5d (%5.2f%%) %s\n' % (bar, count, 100.0 * count / total_count, str(obj)))
+            label = str(obj).rstrip()
+            out.write('%20s %5d (%5.2f%%) %s\n' % (bar, count, 100.0 * count / total_count, label))
 
 
 
@@ -70,20 +96,28 @@ if __name__ == '__main__':
         usage="usage: %s [options] [files]",
         description='Observe data and build a histogram.')
 
+    optparser.add_option(
+        '-L', '--lines',
+        dest='observe_lines', action='store_true', default=False,
+        help='Observe lines instead of words.',
+    )
+    optparser.add_option(
+        '-n', '--limit', metavar='N',
+        dest='limit', type='int', action='store', default=20,
+        help='Limit the histogram to a number of entries.',
+    )
+
     # Get options.
     (options, arguments) = optparser.parse_args()
 
     histogram = ObjectHistogram()
 
     if len(arguments) == 0:
-        histogram.observe_words_from_file(sys.stdin)
-    else:
-        for file_name in arguments:
-            f = open(file_name)
-            histogram.observe_words_from_file(f)
-            f.close()
+        arguments = [sys.stdin]
 
-    histogram.ascii_plot(limit=20)
+    histogram.observe_from_sources(arguments, observe_lines=options.observe_lines)
+
+    histogram.ascii_plot(limit=options.limit)
 
 
 
