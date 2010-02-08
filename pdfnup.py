8aeb18e01c60efb0b7f1816c30a120f8c9f2cc2d
Stefaan Lippens <stefaan.lippens+git@gmail.com>
2010-02-08 01:14:39 +0100

    moved main to top

--- pdfnup.py
+++ pdfnup.py
@@ -18,6 +18,78 @@ import re
 class RequirementException(Exception): pass
 
 
+def main():
+
+    cliparser = build_option_parser()
+
+    # Parse the command line.
+    (clioptions, cliargs) = cliparser.parse_args()
+
+    # Check pdflatex
+    check_requirements(clioptions.pdflatex_bin)
+    
+    # Check options and arguments
+    if len(cliargs) == 0:
+        raise ValueError('At least one input file should be given')
+    if len(cliargs) > 1 and clioptions.output_file != None:
+        raise ValueError('--outfile cannot be used with multiple input files')
+
+    # Set the fitpaper setting and handle the orient setting
+    clioptions.fitpaper = False
+    if clioptions.orientation == 'auto':
+        x, y = parse_nup(clioptions.nup)
+        if x > y:
+            clioptions.orientation = 'landscape'
+        elif x < y:
+            clioptions.orientation = 'portrait'
+        else:
+            clioptions.fitpaper = True
+
+
+    for input_file in cliargs:
+        
+        # TODO: refactor this for loop body?
+        print 'Processing', input_file
+
+        output_file = clioptions.output_file
+        if output_file == None:
+            output_file = '%s-%s.pdf' % (os.path.splitext(input_file)[0], clioptions.nup)
+        assert output_file != input_file
+
+
+        # Create a temp directory for doing our work
+        work_dir = tempfile.mkdtemp(prefix='pdfnuppy')
+        print 'Working in', work_dir
+        # Create a symbolic link to the PDF file to include
+        os.symlink(os.path.abspath(input_file), os.path.join(work_dir, 'input.pdf'))
+        # Generate the LaTeX file.
+        tex_file_name = os.path.join(work_dir, 'pdfnuppy.tex')
+        with file(tex_file_name, 'w') as f:
+            latex_code = generate_tex(clioptions)
+            f.write(latex_code)
+            if clioptions.verbose:
+                print latex_code
+
+        # Call pdflatex (run it in the working directory).
+        p = subprocess.Popen(
+            ['pdflatex', '--interaction', 'batchmode', tex_file_name],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            cwd=work_dir,
+            )
+        p.communicate()
+        # Check if the PDF was generated and copy it over to the output file
+        generated_pdf = os.path.join(work_dir, 'pdfnuppy.pdf')
+        if os.path.isfile(generated_pdf):
+            shutil.copyfile(generated_pdf, output_file)
+            print 'Finished: output is', output_file
+        else:
+            print 'Failed: output file was not written'
+
+        # clean up
+        if clioptions.tidy:
+            shutil.rmtree(work_dir)
+
+
 def check_requirements(pdflatex_bin='pdflatex'):
     '''
     Helper function for checking the requisites and dependencies for pdfnup.
@@ -171,76 +243,6 @@ def build_option_parser():
     return cliparser
 
 
-def main():
-
-    cliparser = build_option_parser()
-
-    # Parse the command line.
-    (clioptions, cliargs) = cliparser.parse_args()
-
-    # Check pdflatex
-    check_requirements(clioptions.pdflatex_bin)
-    
-    # Check options and arguments
-    if len(cliargs) == 0:
-        raise ValueError('At least one input file should be given')
-    if len(cliargs) > 1 and clioptions.output_file != None:
-        raise ValueError('--outfile cannot be used with multiple input files')
-
-    # Set the fitpaper setting and handle the orient setting
-    clioptions.fitpaper = False
-    if clioptions.orientation == 'auto':
-        x, y = parse_nup(clioptions.nup)
-        if x > y:
-            clioptions.orientation = 'landscape'
-        elif x < y:
-            clioptions.orientation = 'portrait'
-        else:
-            clioptions.fitpaper = True
-
-
-    for input_file in cliargs:
-        
-        # TODO: refactor this for loop body?
-        print 'Processing', input_file
-
-        output_file = clioptions.output_file
-        if output_file == None:
-            output_file = '%s-%s.pdf' % (os.path.splitext(input_file)[0], clioptions.nup)
-        assert output_file != input_file
-
-
-        # Create a temp directory for doing our work
-        work_dir = tempfile.mkdtemp(prefix='pdfnuppy')
-        print 'working in', work_dir
-        # Create a symbolic link to the PDF file to include
-        os.symlink(os.path.abspath(input_file), os.path.join(work_dir, 'input.pdf'))
-        # Generate the LaTeX file.
-        tex_file_name = os.path.join(work_dir, 'pdfnuppy.tex')
-        with file(tex_file_name, 'w') as f:
-            latex_code = generate_tex(clioptions)
-            f.write(latex_code)
-            if clioptions.verbose:
-                print latex_code
-
-        # Call pdflatex (run it in the working directory).
-        p = subprocess.Popen(
-            ['pdflatex', '--interaction', 'batchmode', tex_file_name],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-            cwd=work_dir,
-            )
-        p.communicate()
-        # Check if the PDF was generated and copy it over to the output file
-        generated_pdf = os.path.join(work_dir, 'pdfnuppy.pdf')
-        if os.path.isfile(generated_pdf):
-            shutil.copyfile(generated_pdf, output_file)
-            print 'Finished: output is', output_file
-        else:
-            print 'Failed: output file was not written'
-
-        # clean up
-        if clioptions.tidy:
-            shutil.rmtree(work_dir)
 
 
 def parse_nup(nup):
