e50d8f80c70b28925204e93118cacf21c9cf33b3
Stefaan Lippens <stefaan.lippens+git@gmail.com>
2010-10-14 21:05:53 +0200

    added simple tool to pretty print $PATH and $PYTHONPATH

---
+++ path_pretty_print.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+'''
+Simple tool to pretty print the environment
+variable $PATH and the Python path ($PYTHONPATH).
+'''
+
+import os
+import sys
+import pprint
+
+def main():
+    print 'os.environ["PATH"] (aka $PATH):'
+    pprint.pprint(os.environ['PATH'].split(':'))
+
+    print 'sys.path:'
+    pprint.pprint(sys.path)
+
+if __name__ == '__main__':
+    main()
