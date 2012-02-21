8e0b63989c7209e1aa234134a54cad193fdd558b
Stefaan Lippens <stefaan.lippens+git@gmail.com>
2012-02-21 18:38:46 +0100

    refactored collect_file_size_stats() function

--- filelinestats.py
+++ filelinestats.py
@@ -24,16 +24,25 @@ def get_lines(filename):
     return lines
 
 
+def collect_file_size_stats(filename):
+    '''
+    Collect file size stats.
+    '''
+    lines = get_lines(filename)
+    line_lengths = [len(l) for l in lines]
+    max_length = max(line_lengths)
+    average_length = float(sum(line_lengths)) / len(lines)
+    empty_line_fraction = float(len([l for l in lines if l.strip() == ''])) / len(lines)
+
+    return max_length, average_length, empty_line_fraction
+
+
 def main():
 
     filenames = sys.argv[1:]
 
     for filename in filenames:
-        lines = get_lines(filename)
-        line_lengths = [len(l) for l in lines]
-        max_length = max(line_lengths)
-        average_length = float(sum(line_lengths)) / len(lines)
-        empty_line_fraction = float(len([l for l in lines if l.strip() == ''])) / len(lines)
+        max_length, average_length, empty_line_fraction = collect_file_size_stats(filename)
 
         print 'avglen: {avg:5.1f}; maxlen: {max:5d}; empty: {empty:5.2f}%; {filename}: '.format(
             filename=filename,
