c314eb94404c2f70ccd27afb7ed003fba1361303
Stefaan Lippens <soxofaan@gmail.com>
2013-06-29 00:31:25 +0200

    Add exit code awsomeness to print_to_stdout_and_stderr.py

--- print_to_stdout_and_stderr.py
+++ print_to_stdout_and_stderr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 '''
 Print something to standard output and standard error.
+Optionally, you can also specify a status code the process should exit with.
 
 This is an extremely simple script,
 with extreme comment-to-source-code ratio,
@@ -12,3 +13,7 @@ import sys
 
 sys.stdout.write('Hello stdout\n')
 sys.stderr.write('Hello stderr\n')
+
+# Optionally exit with a given status code.
+if len(sys.argv) > 1 and sys.argv[1].isdigit():
+    sys.exit((int)(sys.argv[1]))
